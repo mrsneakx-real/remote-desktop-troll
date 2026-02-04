a8b5d6e8a2e0e3bc3e32eeea8a98c6e2b57a7a49
michael.burri <michaelg.burri@gmail.com>
2026-02-04 16:08:54 +0100

    fixed jar compiler

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -1,10 +1,7 @@
 package main;
 
-import com.sun.tools.javac.Main;
-import javafx.application.Application;
-
 public class Launcher {
-    static void main(String[] args) {
-        Application.launch(AppMain.class, args);
+    public static void main(String[] args) {
+        AppMain.main(args);
     }
 }
